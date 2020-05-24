48171f51f0e48054d0c3c529f16787d10b37af0f
halszka99 <62675854+halszka99@users.noreply.github.com>
2020-05-24 15:14:37 +0200

    Add files via upload

--- test_bench.v
+++ test_bench.v
@@ -1,69 +1,60 @@
 module test();
 
-	wire s0,s1,s2,s3,s4,s5,ov; 	
-	reg x0,x1,x2,x3,x4,x5,y0,y1,y2,y3,y4,y5;
-	reg clk,reset;
-	reg s0_expected,s1_expected,s2_expected,s3_expected,s4_expected,s5_expected,ov_expected;
-	reg[31:0] vectornum, errors; 
-	reg[3:0]  datavectors[10000:0];
+	wire[0:6] s;
+ 	
+	reg[0:5] x;
+	reg[0:5] y;
+	integer i,j,errornum; 
 
-	adder prefix_adder(	.s0(s0),
-				.s1(s1),
-				.s2(s2),
-				.s3(s3),
-				.s4(s4),
-				.s5(s5),
-				.ov(ov),
-				.x0(x0),
-				.x1(x1),
-				.x2(x2),
-				.x3(x3),
-				.x4(x4),
-				.x5(x5),
-				.y0(y0),
-				.y1(y1),
-				.y2(y2),
-				.y3(y3),
-				.y4(y4),
-				.y5(y5)
+	adder prefix_adder(	.s0(s[6]),
+				.s1(s[5]),
+				.s2(s[4]),
+				.s3(s[3]),
+				.s4(s[2]),
+				.s5(s[1]),
+				.ov(s[0]),
+				.x0(x[5]),
+				.x1(x[4]),
+				.x2(x[3]),
+				.x3(x[2]),
+				.x4(x[1]),
+				.x5(x[0]),
+				.y0(y[5]),
+				.y1(y[4]),
+				.y2(y[3]),
+				.y3(y[2]),
+				.y4(y[1]),
+				.y5(y[0])
 			   );
-
-always
-	begin
-		clk=1; #5; clk=0; #5;
-	end
-
-initial            
-	begin
-		$readmemb("data.tv", datavectors); 
-		vectornum= 0; 
-		errors = 0;
-		reset = 1; #27; reset = 0;
-
-	end
-
-always @(posedge clk)
-	begin
-		#1; 
-		{x0,x1,x2,x3,x4,x5,y0,y1,y2,y3,y4,y5, s0_expected, s1_expected, s2_expected, s3_expected, s4_expected, s5_expected, ov_expected} = datavectors[vectornum];
-	end
-
-always @(negedge clk)
-	if (~reset)          
-		begin 
-			if (s0!==s0_expected || s1!==s1_expected || s2!==s2_expected || s3!==s3_expected || s4!==s4_expected || s5!==s5_expected || ov!==ov_expected) 
-				begin  
-					errors = errors + 1;
+	initial 
+		begin
+			{x[0:5]}=0;
+			{y[0:5]}=0;
+			errornum=0; 
+					
+			for(i=0;i<64; i=i+1) 
+				begin
+					{x[0:5]}=i;
+					#1
+					for(j=0;j<64; j=j+1) 
+						begin
+							{y[0:5]}=j; 
+							#1
+							$display("%d%d%d%d%d%d%d",s[0],s[1],s[2],s[3],s[4],s[5],s[6]);
+							if({s[0:6]}!={x[0:5]}+{y[0:5]})
+								begin
+									errornum=errornum+1; 								
+									$display("error");
+									
+								end
+							
+						end
+					 
 				end
-			vectornum= vectornum + 1;
+			
+			$display("\nErrors: %d",errornum);
+					
 		end
-
-initial
-	begin
-		if(errors!==0)
-			$display("ERRORS");
-	end
-
 
 endmodule
 
